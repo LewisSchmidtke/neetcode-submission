add2dcd74e64ac84c030161ec3573a24928dd4ac
Lewis Schmidtke <lewis.schmidtke@t-online.de>
2026-04-13 15:24:34 +0200

    Add: maximum-transactions-without-negative-balance - submission-0

---
+++ Data Structures & Algorithms/maximum-transactions-without-negative-balance/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxTransactions(self, transactions: List[int]) -> int:
+        neg_heap = []
+        heapq.heapify(neg_heap)
+
+        current_balance = 0
+        transaction_count = 0
+
+        for transaction in transactions:
+            current_balance += transaction
+            transaction_count += 1
+
+            if transaction < 0:
+                heapq.heappush(neg_heap, transaction)
+
+            if current_balance < 0:
+                current_balance -= heapq.heappop(neg_heap)
+                transaction_count -= 1
+
+        return transaction_count
+
