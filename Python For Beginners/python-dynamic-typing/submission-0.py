f894cd3a3d7d99a82639e8fba8f3e010939b8eee
Lewis Schmidtke <lewis.schmidtke@t-online.de>
2026-07-24 10:16:44 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.8
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "LACHS"
+print(type(variable))
+variable = ["LACHS","FISCH"]
+print(type(variable))
